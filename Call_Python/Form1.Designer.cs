a9924c3c5d5c9847ac1b068f8509028062cc5bd4
snc29654 <snc29654l@nifty.com>
2022-03-14 10:04:12 +0900

    ボタン横に説明追加

--- Call_Python/Form1.Designer.cs
+++ Call_Python/Form1.Designer.cs
@@ -32,13 +32,17 @@ namespace Call_Python
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(276, 52);
+            this.button1.Location = new System.Drawing.Point(12, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(153, 46);
+            this.button1.Size = new System.Drawing.Size(134, 34);
             this.button1.TabIndex = 0;
             this.button1.Text = "MP4_play";
             this.button1.UseVisualStyleBackColor = true;
@@ -46,9 +50,10 @@ namespace Call_Python
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(278, 122);
+            this.button2.Location = new System.Drawing.Point(13, 64);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(150, 44);
+            this.button2.Size = new System.Drawing.Size(131, 33);
             this.button2.TabIndex = 1;
             this.button2.Text = "list_jpg_disp";
             this.button2.UseVisualStyleBackColor = true;
@@ -56,25 +61,58 @@ namespace Call_Python
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(280, 194);
+            this.button3.Location = new System.Drawing.Point(15, 118);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(149, 43);
+            this.button3.Size = new System.Drawing.Size(130, 32);
             this.button3.TabIndex = 2;
             this.button3.Text = "select_jpg_disp";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(161, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "複数のMP4を同時再生";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(164, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "jpgファイルをリストから選択表示";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "jpgを自動再生";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(858, 477);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -83,6 +121,9 @@ namespace Call_Python
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
     }
 }
 
