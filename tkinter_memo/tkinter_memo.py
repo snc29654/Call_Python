3fd3936861a3f000c0ea69074d57a5de9ea56e42
snc29654 <snc29654l@nifty.com>
2022-10-12 09:55:34 +0900

    機能選択をラジオボタンで選択

--- tkinter_memo/tkinter_memo.py
+++ tkinter_memo/tkinter_memo.py
@@ -33,6 +33,25 @@ btnRead=tkinter.Button(root, height=1, width=10, text="Clear",
                     command=getTextInput)
 
 
+
+def execute():
+    func =function.get()
+    if(func==1):
+        btn_click2()
+    if(func==2):
+        btn_click10()
+    if(func==3):
+        btn_click8()
+    if(func==4):
+        btn_click9()
+    if(func==5):
+        btn_click4()
+    if(func==6):
+        btn_click2_sc()
+    if(func==7):
+        btn_click5()
+
+    
 def btn_click6():
     global fontsize
     fontsize = fontsize + 1
@@ -109,7 +128,7 @@ def btn_click10():
                 print(row)
                 print(type(row))
                 text = "-".join(map(str, row))
-                if(px_v.get()==2):
+                if(header_only.get()==2):
                 #if combovalue =='header':
                     text=text[:80]
 
@@ -345,7 +364,7 @@ def btn_click5():
 # ボタン
 btn = tkinter.Button(root, text='キーワード検索', command=btn_click)
 btn.place(x=300, y=10)
-
+"""
 btn2 = tkinter.Button(root, text='追加', command=btn_click2)
 btn2.place(x=10, y=110)
 
@@ -357,13 +376,13 @@ btn4.place(x=10, y=150)
 
 btn5 = tkinter.Button(root, text='全削除', command=btn_click5)
 btn5.place(x=10, y=180)
-
+"""
 btn6 = tkinter.Button(root, text='フォント大', command=btn_click6)
 btn6.place(x=10, y=210)
 
 btn7 = tkinter.Button(root, text='フォント小', command=btn_click7)
 btn7.place(x=10, y=240)
-
+"""
 btn8 = tkinter.Button(root, text='キー指定表示', command=btn_click8)
 btn8.place(x=10, y=270)
 
@@ -376,6 +395,8 @@ btn10.place(x=10, y=330)
 
 btn11 = tkinter.Button(root, text='scraping追加', command=btn_click2_sc)
 btn11.place(x=10, y=360)
+
+"""
 
 
 # 画面サイズ
@@ -429,14 +450,14 @@ test_combobox.current(0)
 test_combobox.pack()
 """
 
-px_v = tkinter.IntVar(value=1)
+header_only = tkinter.IntVar(value=1)
 
 
 px_radio_1 = tkinter.Radiobutton(
     root,
     text="全て",
     value=1,
-    variable=px_v
+    variable=header_only
 )
 px_radio_1.place(x=700, y=30)
 
@@ -444,9 +465,68 @@ px_radio_2 = tkinter.Radiobutton(
     root,
     text="ヘッダーのみ",
     value=2,
-    variable=px_v
+    variable=header_only
 )
 px_radio_2.place(x=800, y=30)
+
+
+function = tkinter.IntVar(value=1)
+
+
+func_radio_1 = tkinter.Radiobutton(
+    root,
+    text="追加",
+    value=1,
+    variable=function
+)
+func_radio_1.place(x=700, y=60)
+
+func_radio_2 = tkinter.Radiobutton(
+    root,
+    text="全表示",
+    value=2,
+    variable=function
+)
+func_radio_2.place(x=700, y=80)
+
+func_radio_3 = tkinter.Radiobutton(
+    root,
+    text="キー指定表示",
+    value=3,
+    variable=function
+)
+func_radio_3.place(x=700, y=100)
+func_radio_4 = tkinter.Radiobutton(
+    root,
+    text="キー指定更新",
+    value=4,
+    variable=function
+)
+func_radio_4.place(x=700, y=120)
+func_radio_5 = tkinter.Radiobutton(
+    root,
+    text="キー指定削除",
+    value=5,
+    variable=function
+)
+func_radio_5.place(x=700, y=140)
+func_radio_6 = tkinter.Radiobutton(
+    root,
+    text="スクレーピング",
+    value=6,
+    variable=function
+)
+func_radio_6.place(x=700, y=160)
+func_radio_7 = tkinter.Radiobutton(
+    root,
+    text="全削除",
+    value=7,
+    variable=function
+)
+func_radio_7.place(x=700, y=180)
+
+btn12 = tkinter.Button(root, text='実行', command=execute)
+btn12.place(x=700, y=250)
 
 
 # 表示
